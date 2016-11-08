0691331eaf24788d793eb4242e38b5fef78b44a1
C. Tegho <ct506@mlsalt-cpu2.eng.cam.ac.uk>
2016-11-08 01:15:05 +0000

    moved to MATLAB

---
+++ emp_ratio.m
@@ -0,0 +1,15 @@
+function P = emp_ratio()
+% Ranking of each player based on the empirical ratio of number of wins to
+% total number of games played
+
+load("tennis_data.mat");
+P = zeros(length(W),1);
+for k = 1:length(W)
+  games = sum(G == k);
+  gamesplayed = games(:,1)+games(:,2);
+  if (gamesplayed!=0)
+    P(k) = games(:,1)/gamesplayed;
+  end
+end
+
+cw2(P,W);
